9b1e44a708ad42444eafd5e5ba7625ab27ad8b28
Vince Elizaga <vncelizaga@gmail.com>
2015-12-28 18:10:13 +0800

    add get started redirection to app

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#invalidEmail').hide();
+	$('#continueButton').click(function(){
+		var url = "http://app.adzoco.com/#/signup-pop?username=" + $('#userEmail').val() + "&firstName=none&lastName=none&phoneNumber=none";
+		var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+		if (testEmail.test($('#userEmail').val())){
+			window.location.replace(url);
+			$('#invalidEmail').hide();
+		}
+		else{
+			$('#invalidEmail').show();
+		}
+
+	});
+
+});
