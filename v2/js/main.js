df6e3887d57c8a4245b51ff1e384eb28e730f8c2
Vince Elizaga <vncelizaga@gmail.com>
2015-12-28 18:30:01 +0800

    refactor: change to form on submit

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#invalidEmail').hide();
-	$('#continueButton').click(function(){
+	$('#getStartedForm').submit(function(){
 		var url = "http://app.adzoco.com/#/signup-pop?username=" + $('#userEmail').val() + "&firstName=none&lastName=none&phoneNumber=none";
 		var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
 		if (testEmail.test($('#userEmail').val())){
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		else{
 			$('#invalidEmail').show();
 		}
-
+		return false;
 	});
 
 });
